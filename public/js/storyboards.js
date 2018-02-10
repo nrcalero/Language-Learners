bbf7bce0dbbd4bd1ff977af2fb467acd0cda692d
Nathanyel Calero <ncalero@ucsd.edu>
2018-02-09 18:23:17 -0800

    Fixed mobile issues, cleaned JS

--- public/js/storyboards.js
+++ public/js/storyboards.js
@@ -1,24 +1,30 @@
-'use strict';
+function init() {
+  'use strict';
+  return true;
+}
 
-$(document).ready(function() {
-	initializePage();
-	$(".next-btn").click(function() {
-		console.log("button clicked");
-	event.preventDefault();
-	$(".storystyle p").text("The story will change when the 'Next Story' button is clicked.");
+$(document).ready(function () {
+  initializePage();
+
+  /* 'Next Story' listener */
+  $('#next').click(function () {
+    console.log('Button clicked');
+    event.preventDefault();
+    $('.story p').text('The story will change when the "Next Story" button is clicked.');
+  });
+
+  /* 'Word List' Listener */
+  $('#words').click(function () {
+    event.preventDefault();
+    alert('Popup with words in the text will appear!');
+  });
 });
-	$(".word-btn").click(function() {
-		event.preventDefault();
-
-		alert("Popup with words in the text will appear!");
-	});
-})
 
 function initializePage() {
-	console.log("Javascript connected!");
+  console.log('Javascript connected!');
 }
 
 function changeInterest() {
-	var interest = document.getElementById("interests").value;
-	$(".storystyle p").text("This new story will be about " + interest + "!");
+  var interest = document.getElementById('interests').value;
+  $('.story p').text('This story will be about ' + interest + '!');
 }

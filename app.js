62c960d14a86069c5511f5d5b39dfc68104f5291
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-02-12 20:16:27 -0800

    Attempt to fix heroku

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get('/changedifficulty', changedifficulty.changeDifficulty)
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get(process.env.PORT || 3000), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });

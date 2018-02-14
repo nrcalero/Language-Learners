6694d95dcdd7b99c1aac97e28444172886e68704
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-02-13 17:25:21 -0800

    finished storyboard functionality

--- public/js/storyboards.js
+++ public/js/storyboards.js
@@ -15,12 +15,7 @@ $(document).ready(function () {
   
 
   /* 'Word List' Listener */
-  $('#words').click(function () {
-    event.preventDefault();
-    alert('Popup with words in the text will appear!');
 
-
-  });
 });
 
 
@@ -28,24 +23,56 @@ $(document).ready(function () {
 function initializePage() {
   var usedstories = makeUsedStories();
   var interest = document.getElementById('interests').value;
-  console.log(index);
+  $("#text").empty();
+  $("#text").append('<p> <em><strong> Word list: </strong></em> </p>');
   if (interest == "sports") {
     $('.story p').text(usedstories.sports[++index % maxsize].body);
+    $('#wordlist').text(usedstories.sports[index % maxsize].words);
+
+    Object.keys(usedstories.sports[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.sports[index % maxsize].words[0][key] + '</p>');
+    });
+
   }
   if (interest == "fooddrink") {
     $('.story p').text(usedstories.fooddrink[++index % maxsize].body);
+    console.log(JSON.stringify(usedstories.fooddrink[index % maxsize].words[0]));
+    Object.keys(usedstories.fooddrink[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.fooddrink[index % maxsize].words[0][key] + '</p>');
+    });
   }
   if (interest == "news") {
     $('.story p').text(usedstories.news[++index % maxsize].body);
+    $('#wordlist').text(usedstories.news[index % maxsize].words);
+    Object.keys(usedstories.news[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.news[index % maxsize].words[0][key] + '</p>');
+    });
+
   }
   if (interest == "music") {
     $('.story p').text(usedstories.music[++index % maxsize].body);
+    $('#wordlist').text(usedstories.music[index % maxsize].words);
+    Object.keys(usedstories.music[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.music[index % maxsize].words[0][key] + '</p>');
+    });
+
+
   }
   if (interest == "healthfitness") {
     $('.story p').text(usedstories.healthfitness[++index % maxsize].body);
+    $('#wordlist').text(usedstories.healthfitness[index % maxsize].words);
+    Object.keys(usedstories.healthfitness[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.healthfitness[index % maxsize].words[0][key] + '</p>');
+    });
+
   }
   if (interest == "technology") {
     $('.story p').text(usedstories.technology[++index % maxsize].body);
+    $('#wordlist').text(usedstories.technology[index % maxsize].words);
+    Object.keys(usedstories.technology[index % maxsize].words[0]).forEach(function(key) {
+      $('#text').append('<p>' + key + ' : ' + usedstories.technology[index % maxsize].words[0][key] + '</p>');
+    });
+
   }
 }
 
@@ -88,4 +115,12 @@ function getInterestData() {
   return result;
 }
 
+function on() {
+  event.preventDefault();
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
 

2d645fd602255e2abe1934358cdc6641adc53541
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-02-17 17:32:35 -0800

    overflow

--- public/js/difficulty.js
+++ public/js/difficulty.js
@@ -7,15 +7,11 @@ $(document).ready(function () {
   initializePage();
   $('.switch #sports:checkbox').on("change", function () {
     var data = $.getJSON("interestsdata", function (mydata) {
-          console.log(mydata.Interests[0]);
           if (mydata.Interests[0].enabled == null) {
             mydata.Interests[0].enabled = "checked";
           }
           else {
-                        console.log(JSON.stringify(mydata.Interests[0]));
-
             mydata.Interests[0].enabled = null;
-            console.log(JSON.stringify(mydata.Interests[0]));
           }
 
     });

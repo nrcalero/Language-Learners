45d56e07403fc1ccf3f3698d67c31ed16ad2e46d
Nathanyel Calero <ncalero@ucsd.edu>
2018-02-22 14:00:22 -0800

    Minor aesthetic changes

--- public/js/words.js
+++ public/js/words.js
@@ -5,7 +5,7 @@ function init() {
 
 function changeWords() {
   var input = document.getElementById('wordSearch').value.toUpperCase();
-  var words = document.getElementById('words').getElementsByTagName('p');
+  var words = document.getElementById('words').getElementsByTagName('li');
 
   for (var w in words) {
     var id = words[w].id.toUpperCase();

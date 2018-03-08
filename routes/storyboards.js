33c1f07174fcce295bae93dfb8f703e86388416e
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-03-08 15:27:11 -0800

    added page B

--- routes/storyboards.js
+++ routes/storyboards.js
@@ -32,3 +32,32 @@ exports.viewAlt = function (req, res) {
   var interestJSON = { Interests: interestlist };
   res.render('storyboardsAlt', interestJSON);
 };
+
+exports.viewAlt = function (req, res) {
+
+  var interestlist = [];
+  var index = 0;
+  for (var key in interests.Interests) {
+    if (interests.Interests[key].enabled == 'checked') {
+      interestlist[index] = interests.Interests[key];
+      ++index;
+    }
+  }
+
+  var interestJSON = { Interests: interestlist };
+  res.render('storyboards', interestJSON);
+};
+
+exports.viewAlt = function (req, res) {
+  var interestlist = [];
+  var index = 0;
+  for (var key in interests.Interests) {
+    if (interests.Interests[key].enabled == 'checked') {
+      interestlist[index] = interests.Interests[key];
+      ++index;
+    }
+  }
+
+  var interestJSON = { Interests: interestlist };
+  res.render('storyboardsOLD', interestJSON);
+};

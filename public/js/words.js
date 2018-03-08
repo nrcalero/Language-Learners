45fd892125877aa5992a96af988a3d36e4ec4b62
Nathanyel Calero <ncalero@ucsd.edu>
2018-03-08 10:23:32 -0800

    Fixed JavaScript code

--- public/js/words.js
+++ public/js/words.js
@@ -7,13 +7,13 @@ function changeWords() {
   var input = document.getElementById('wordSearch').value.toUpperCase();
   var words = document.getElementById('words').getElementsByTagName('li');
 
-  for (var w in words) {
-    var content = words[w].textContent.toUpperCase();
+  for (i = 0; i < words.length; i++) {
+    var content = words[i].textContent.toUpperCase();
 
     if (content.indexOf(input) > -1) {
-      words[w].style.display = '';
+      words[i].style.display = '';
     } else {
-      words[w].style.display = 'none';
+      words[i].style.display = 'none';
     }
   }
 }
